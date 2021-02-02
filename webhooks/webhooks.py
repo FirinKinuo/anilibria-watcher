9b62948a6fb34daa9104b92a6499fc9f7147e641
FirinKinuo <firinkinuo@protonmail.ch>
2021-02-03 02:54:29 +0300

    added the ability to work with lists of links and content

--- webhooks/webhooks.py
+++ webhooks/webhooks.py
@@ -2,17 +2,32 @@ import requests
 from .integrations import find_integrations_and_send
 
 
-def send_webhook(webhook_url: (str, list), content: dict) -> None:
+def _send(link: str, content: (dict, list)) -> None:
+    """
+    Служебная функция для отправки вебхуков
+    :param link: Ссылка на вебхук
+    :param content: Словарь или список словарей с контентом для отправки вебхуков
+    :return: None
+    """
+    if isinstance(content, dict):  # Если content - словарь, то отправляем прям так
+        if not find_integrations_and_send(link, content):
+            requests.post(link, data={'content': f'{content}'})
+    elif isinstance(content, list):  # Иначе, если  content - список, то отправляем его каждый элемент
+        for data in content:
+            if not find_integrations_and_send(link, data):
+                requests.post(link, data={'content': f'{data}'})
+
+
+def send_webhook(webhook_url: (str, dict), content: (dict, list)) -> None:
     """
     Отправляет вебхук по указанной ссылке
     :param webhook_url: Ссылка на Webhook пользователя или список ссылок
-    :param content: dict с данными для отправки через webhook
+    :param content: Список словарей или словарь с данными для отправки через webhook
     :return: None
     """
+
     if isinstance(webhook_url, str):
-        if not find_integrations_and_send(webhook_url, content):
-            requests.post(webhook_url, data={'content': f'{content}'})
-    elif isinstance(webhook_url, list):
-        for link in webhook_url:
-            if not find_integrations_and_send(link, content):
-                requests.post(link, data={'content': f'{content}'})
+        _send(webhook_url, content)
+    elif isinstance(webhook_url, dict):
+        for link in webhook_url['links']:
+            _send(link, content)
