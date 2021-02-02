0e7dea819a7061b066fbe9eac806b9f5db9ce31c
FirinKinuo <firinkinuo@protonmail.ch>
2021-02-03 02:47:02 +0300

    changed the definition of the last series by date to definition by reference, it works more reliably, fixed the definition of the current series

--- watcher/rss.py
+++ watcher/rss.py
@@ -1,5 +1,4 @@
 import feedparser
-import datetime
 
 
 def _find_current_episode(dirty_str: str) -> int:
@@ -10,56 +9,44 @@ def _find_current_episode(dirty_str: str) -> int:
     """
 
     try:
-        return int(dirty_str.split('-')[1][0])
+        return int(dirty_str.split('-')[1][:dirty_str.split('-')[1].find(" ")])
     except IndexError:
         # Если в тайтле вышла первая серия, то поиск разделением не получится
         return int(dirty_str[dirty_str.find(":")+1:dirty_str.find("[")].replace(" ", ""))
 
 
-def _filter_last_anime(rss: list, last_title_date: str) -> list:
+def _filter_last_anime(rss: list, last_title_link: str) -> list:
     """
     Возращает отфильтрованный до последнего тайтла в записи список аниме
     :param rss: Список RSS для фильтрации
-    :param last_title_date: Дата последнего тайтла
+    :param last_title_link: Ссылка на скачивание последнего тайтла
     :return: Отфильтрованный список RSS
     """
 
-    """
-    Немного пояснения за следующий костыль:
-    Каким то магическим образом, строка из RSS
-    Не является идентичной такой же строке из БД, поэтому последующий код не имел смысла и он тупо
-    Отправлял все, что было в RSS, от отделяя новое от старого, приведение к типу datetime
-    Позволило божественному Python понять, что даты то одинаковые, че дальше шакалиться
-    """
-
-    date_in_db = datetime.datetime.strptime(last_title_date, '%Y-%m-%d %H:%M:%S')
-
     for i, el in enumerate(rss):
-        date_in_rss = datetime.datetime.strptime(el.published, '%Y-%m-%d %H:%M:%S')
-        if date_in_rss == date_in_db:
+        if el.links[-1].href == last_title_link:
             return rss[:i]
     return rss
 
 
-def parse_anilibria_rss(last_title_date=None, filter_last=False) -> (list, bool):
+def parse_anilibria_rss(last_title_link=None, filter_last=False) -> (list, None):
     """
     Получить готовый список с данными по последним 30 тайтлам в списке
 
-    :param last_title_date: Дата добавления последнего тайтла для проверки на обновление списка
+    :param last_title_link: Ссылка на скачивание последнего тайтла для проверки на обновление списка
     :param filter_last: Проводить ли фильтрацию до последнего записанного тайтла, необходим last_title_date
-    :return: Возвращает список последних в RSS тайтлов, либо False, если нет новых тайтлов
+    :return: Возвращает список последних в RSS тайтлов, либо None, если нет новых тайтлов
     """
 
     rss = feedparser.parse("https://dark-libria.it/rss.xml").entries
 
-    if last_title_date is not None:
-        last_title_date = datetime.datetime.strftime(last_title_date, '%Y-%m-%d %H:%M:%S')
+    if last_title_link is not None:
 
-        if last_title_date == rss[0].published:
-            return False
+        if last_title_link == rss[0].links[-1].href:
+            return None
 
     if filter_last:
-        rss = _filter_last_anime(rss, last_title_date)
+        rss = _filter_last_anime(rss, last_title_link)
 
     parsed_data = list()
 
