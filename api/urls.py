3df4fa4236d1eb3ec771519b5c8ac87efed2a0cd
FirinKinuo <firinkinuo@protonmail.ch>
2021-03-14 21:47:32 +0300

    create an api to get information about the user and his favorites, jwt authorization

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 from .views import *
 
 
 urlpatterns = [
     path("auth/", obtain_jwt_token),
-    path("anime/", AllAnimeView.as_view()),
-    path("anime/<int:pk>", AnimeView.as_view())
+    path("auth/verify/", verify_jwt_token),
+    path("user/<int:pk>/", UserDataView.as_view()),
+    path("user/<int:pk>/favorites/", UserFavoriteListView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,16 +1,33 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from .serializer import *
+from .logic import *
+from .serializers import *
 from watcher import models
 
 
-class AllAnimeView(ListAPIView):
-    queryset = models.AnimeDownloadLink.objects
-    serializer_class = AnimeDownloadLinkSerializer
+class UserFavoriteListView(ListAPIView):
+    """
+    Контроллер, отвечающий за взаимодействие с моделью UserFavorite
+    """
+    serializer_class = UserFavoriteSerializer
+    queryset = models.UserFavorite.objects
+    permission_classes = (IsAuthenticated,)
+
+    # GET запрос на получение данные об избранном пользователя
+    def get(self, request, *args, **kwargs):
+        return return_user_favorites(self, request, *args, **kwargs)
 
 
-class AnimeView(RetrieveAPIView):
-    queryset = models.AnimeDownloadLink.objects
-    serializer_class = AnimeDownloadLinkSerializer
+class UserDataView(RetrieveAPIView):
+    """
+    Контроллер, отвечающий за взаимодействие с моделью User
+    """
+    serializer_class = UserSerializer
+    queryset = models.User
+    permission_classes = (AllowAny,)
 
+    # GET запрос на получение данные о пользователе
+    def get(self, request, *args, **kwargs):
+        return return_user_data(self, request, *args, **kwargs)
 
